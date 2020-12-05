3780ccf3d068699545c830cefc3c378242b28e7d
Cobanov <mertcobanov@gmail.com>
2020-12-05 16:08:15 +0300

    wiping feauter

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 import datetime
 from flask import Flask, render_template, request
+from flask_basicauth import BasicAuth
 import database
+import os
 
 
 app = Flask(__name__)
 
 database.create_tables()
+token = os.getenv("password")
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -19,3 +23,14 @@ def home():
             pass
 
     return render_template("home.html", entries=database.retrieve_entries())
+
+
+@app.route("/wipe", methods=["GET", "POST"])
+def wipe():
+    if request.method == "POST":
+        password = request.form.get("password")
+        if password == token:
+            database.clear_database()
+            return render_template("home.html", entries=database.retrieve_entries())
+
+    return render_template("wipe.html")
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlite3
 CREATE_TABLE = "CREATE TABLE IF NOT EXISTS entries (content TEXT NOT NULL UNIQUE, date TEXT)"
 CREATE_ENTRY = "INSERT INTO entries VALUES (?, ?)"
 RETRIEVE_ENTRIES = "SELECT * FROM entries ORDER BY date ASC"
-
+WIPE_ENTRIES = "DROP TABLE entries"
 
 def create_tables():
     with sqlite3.connect("data.db") as connection:
@@ -20,3 +20,9 @@ def retrieve_entries():
         cursor = connection.cursor()
         cursor.execute(RETRIEVE_ENTRIES)
         return cursor.fetchall()
+
+def clear_database():
+    with sqlite3.connect("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute(WIPE_ENTRIES)
+        connection.execute(CREATE_TABLE)
