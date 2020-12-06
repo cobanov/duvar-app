c214ba2f67da5c9887cb741fd9e794d6c07d607e
Mert Cobanov <mertcobanov@gmail.com>
2020-12-06 20:53:29 +0300

    database changes

--- database.py
+++ database.py
@@ -6,6 +6,8 @@ RETRIEVE_ENTRIES = "SELECT * FROM entries"
 RETRIEVE_ENTRIES_TOP = "SELECT * FROM entries ORDER BY vote ASC"
 WIPE_ENTRIES = "DROP TABLE entries"
 UPVOTE = "UPDATE entries SET vote = vote + 1 WHERE message_id = (?)"
+DELETE_ENTRY = "DELETE FROM entries WHERE message_id = (?)"
+
 message_id = 1
 
 def create_tables():
@@ -15,11 +17,31 @@ def create_tables():
 
 def create_entry(content, date):
     global message_id
+    print(message_id)
+    print(type(message_id))
+
     with sqlite3.connect("data.db") as connection:
-        connection.execute(CREATE_ENTRY, (message_id, content, date, 0))
-    message_id += 1
+        
+            try:
+                connection.execute(CREATE_ENTRY, (message_id, content, date, 0))
+                message_id += 1
+                
+    
+            except Exception as e:  
+                if "message_id" in str(e):
+                    while True:
+                        try:               
+                            connection.execute(CREATE_ENTRY, (message_id, content, date, 0))
+                            message_id += 1
+                            break
 
-
+                        except:
+                            message_id += 1
+                        
+            
+    
+                
+            
 def retrieve_entries():
     with sqlite3.connect("data.db") as connection:
         cursor = connection.cursor()
@@ -35,8 +57,12 @@ def retrieve_entries_top():
 def upvote(message_id):
     with sqlite3.connect("data.db") as connection:
         cursor = connection.cursor()
-        
         cursor.execute(UPVOTE, (message_id, ))
+
+def delete(message_id):
+    with sqlite3.connect("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute(DELETE_ENTRY, (message_id, ))
 
 def clear_database():
     with sqlite3.connect("data.db") as connection:
