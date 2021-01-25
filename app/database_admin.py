c6bc3faf2b40055516792a876c57b4c38c23ce14
Mert Cobanov <mertcobanov@gmail.com>
2021-01-26 01:30:31 +0300

    db

---
+++ app/database_admin.py
@@ -0,0 +1,31 @@
+import psycopg2
+import os
+
+
+class DuvarovDB():
+    def __init__(self) -> None:
+        self._database = "d5ggjt80j3jic5"
+        self._user = "lewormdmwvpmrv"
+        # self.password = os.getenv("duvarov_password")
+        self._host = "ec2-18-214-211-47.compute-1.amazonaws.com"
+
+    def connect_db(self, password):
+        self.connection = psycopg2.connect(database=self._database, user=self._user,
+                                           password=password, host=self._host, port="5432")
+
+    def get_data(self,  query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        self.connection.close()
+        return data
+
+
+mert = DuvarovDB()
+mert.connect_db(os.getenv('db_password'))
+query = """SELECT * FROM duvarapp LIMIT 10"""
+
+data = mert.get_data(query)
+
+for datum in data:
+    print(datum)
